6947318377232fd85306c6321db58b030c796da6
Igor Shargin <igor20513@gmail.com>
2023-06-06 00:06:22 +0500

    Reversed sequence solve 2.0

--- tasks/reversedSequence8kyu.js
+++ tasks/reversedSequence8kyu.js
@@ -1,10 +1,6 @@
 //https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/javascript
 const reverseSeq = n => {
-    let res = []
-    for (let i = n; i > 0; i--) {
-        res.push(i)
-    }
-    return res
+    return new Array(5).fill(n).map((el, i) => el - i)
 };
 
 console.log(reverseSeq(5)) //[5, 4, 3, 2, 1]
